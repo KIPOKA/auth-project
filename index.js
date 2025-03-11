5ea8a24cde866bbfe5c9a58a2ffe5551515f6b73
KIPOKA <ladykipoka@gmail.com>
2025-03-11 23:02:37 +0200

    updated version 2

--- index.js
+++ index.js
@@ -8,16 +8,18 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
-app.use(cors);
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
+
 // Endpoint to test the server
 app.get("/", (req, res) => {
   res.json({ message: "Hello from the server" });
 });
 
+
 // Get the port from .env or use a default value
 const PORT = process.env.PORT || 5000;
 
-app.listen(process.env.PORT, () => {
-  console.log(`Listening ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
